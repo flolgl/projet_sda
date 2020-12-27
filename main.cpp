111e63a633906081ab3e97d510a5b183b6fb516b
Syltenor <74373766+Syltenor@users.noreply.github.com>
2020-12-27 22:37:23 +0100

    Sprint 3 (pas testé)
    Besoin des header et sources du tp 2 autres que main

--- main.cpp
+++ main.cpp
@@ -18,6 +18,25 @@ void sprint2() {
 	supprimer(liste);
 }
 
+void sprint3() {
+	// Déclaration des deux listes utilisées 
+	Liste_mot liste1;
+	Liste_mot liste2;
+	// Initialisation, déclaration et tri de la première liste
+	initialiser_liste_mot(liste1, 50);
+	getMots_trie(liste1);
+	trie(liste1);
+	// Initialisation, déclaration et tri de la deuxième liste
+	initialiser_liste_mot(liste2, 50);
+	getMots_trie(liste2);
+	trie(liste2);
+	// Affichage des mots de la deuxième liste qui ne sont pas présents dans la première
+	test_presence(liste1, liste2);
+	// Libération des espaces mémoire des deux listes
+	supprimer(liste1);
+	supprimer(liste2);
+}
+
 int main() {
 	int num;
 	std::cin >> num;
@@ -25,6 +44,8 @@ int main() {
 	case 1:
 		sprint1(); break; // correspond au main du premier programme
 	case 2:
-		sprint2(); break; // correspond au main du second programme 
+		sprint2(); break; // correspond au main du second programme
+	case 3:
+		sprint3(); break; // correspond au main du troisième programme
 	}
 }
---
+++ test_presence.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "liste_mot.h"
+
+void test_presence(Liste_mot liste1, Liste_mot liste2);
