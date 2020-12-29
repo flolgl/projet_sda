9899e6cc1fbc432a963d9e3a882a8811b00bd6c2
flowdevLOL <48724807+flowdevLOL@users.noreply.github.com>
2020-12-29 12:11:19 +0100

    Fix sprint 3, non utilisation des ref ou pointeur

--- test_presence.h
+++ test_presence.h
@@ -2,5 +2,5 @@
 
 #include "liste_mot.h"
 
-void test_presence(Liste_mot liste1, Liste_mot liste2);
+void test_presence(Liste_mot& liste1, Liste_mot& liste2);
 void test_mots_communs(Liste_mot& liste1, Liste_mot& liste2, Liste_mot& liste_result);
